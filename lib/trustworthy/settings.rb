d5e1c9bd4e1802751fd8c9887d5335cd8587d60e
John Downey <jdowney@gmail.com>
2012-09-15 11:08:15 -0700

    Switch setting storage to use YAML::Store

--- lib/trustworthy/settings.rb
+++ lib/trustworthy/settings.rb
@@ -2,15 +2,12 @@ module Trustworthy
   class Settings
     attr_reader :keys, :secrets
 
-    def self.load(filename)
-      data = File.read(filename)
-      yaml = YAML.load(data)
-      new(yaml['keys'], yaml['secrets'])
-    end
-
-    def initialize(keys = {}, secrets = {})
-      @keys = keys
-      @secrets = secrets
+    def initialize(filename)
+      @store = YAML::Store.new(filename)
+      @store.transaction do
+        @keys = @store.fetch(:keys, {})
+        @secrets = @store.fetch(:secrets, {})
+      end
     end
 
     def add_key(key, username, password)
@@ -21,9 +18,9 @@ module Trustworthy
       ciphertext = crypto.encrypt(plaintext)
 
       @keys[username] = {
-        'salt' => salt,
-        'ciphertext' => ciphertext,
-        'authentication' => crypto.sign(ciphertext)
+        :salt => salt,
+        :ciphertext => ciphertext,
+        :authentication => crypto.sign(ciphertext)
       }
     end
 
@@ -33,20 +30,22 @@ module Trustworthy
 
     def unlock_key(username, password)
       key_data = keys[username]
-      ciphertext = key_data['ciphertext']
-      signature = key_data['authentication']
-      salt = key_data['salt']
+      ciphertext = key_data[:ciphertext]
+      signature = key_data[:authentication]
+      salt = key_data[:salt]
+
       crypto = _crypto_from_password(salt, password)
       raise 'invalid signature' unless crypto.valid_signature?(signature, ciphertext)
+
       plaintext = crypto.decrypt(ciphertext)
       x, y = plaintext.split(',').map { |n| BigDecimal.new(n) }
       Trustworthy::Key.new(x, y)
     end
 
     def write(filename)
-      File.open(filename, 'w') do |file|
-        data = YAML.dump('keys' => @keys, 'secrets' => @secrets)
-        file.write(data)
+      @store.transaction do
+        @store[:keys] = @keys
+        @store[:secrets] = @secrets
       end
     end
 
--- trustworthy.gemspec
+++ trustworthy.gemspec
@@ -21,7 +21,6 @@ Gem::Specification.new do |s|
   s.add_dependency 'hkdf', '~> 0.1.0'
   s.add_dependency 'posix-spawn', '~> 0.3.6'
   s.add_dependency 'scrypt', '~> 1.1'
-  s.add_development_dependency 'fakefs', '~> 0.4.0'
   s.add_development_dependency 'rspec', '~> 2.11'
   s.add_development_dependency 'rake'
 end
